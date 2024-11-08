5a856d1cd2f0d065b0968265034c29b0a71dedb0
NghiaDiep5199 <dinhthekiet5199@gmail.com>
2024-11-08 22:12:02 +0700

    Thuc hanh

---
+++ WEB_QLBANSACH_TH/Controllers/BookStoreController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEB_QLBANSACH_TH.Models;
+
+namespace WEB_QLBANSACH_TH.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        QLBANSACHEntities data = new QLBANSACHEntities();
+        // GET: BookStore
+        private List<SACH> Laysachmoi(int count)
+        {
+            // Sap xep sach giam dan theo ngay cap nhat
+            return data.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        // GET: BookStore
+        public ActionResult Index()
+        {
+            var sachmoi = Laysachmoi(5);
+            return View(sachmoi);
+        }
+
+        public ActionResult ChuDe()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+
+        public ActionResult NhaXuatBan()
+        {
+            var nhaxb = from cd in data.NHAXUATBANs select cd;
+            return PartialView(nhaxb);
+        }
+
+        public ActionResult SPTheoChuDe(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+
+        public ActionResult SPTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaNXB == id select s;
+            return View(sach);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var sach = from s in data.SACHes where s.Masach == id select s;
+            return View(sach.Single());
+        }
+    }
+}
---
+++ WEB_QLBANSACH_TH/Models/GioHang.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB_QLBANSACH_TH.Models
+{
+    public class GioHang
+    {
+        //Tao doi tuong data chua dữ liệu từ model dbBansach đã tạo.
+        QLBANSACHEntities data = new QLBANSACHEntities();
+
+        public int iMasach { set; get; }
+              
+        public string sTensach { set; get; }
+      
+        public string sAnhbia { set; get; }
+      
+        public Double dDongia { set; get; }
+             
+        public int iSoluong { set; get; }
+               
+        public Double dThanhtien
+        {
+            get { return iSoluong * dDongia; }
+        }
+        //Khoi tao gio hàng theo Masach duoc truyen vao voi Soluong mac dinh la 1
+        public GioHang(int Masach)
+        {
+            iMasach = Masach;
+            SACH sach = data.SACHes.Single(n => n.Masach == iMasach);
+            sTensach = sach.Tensach;
+            sAnhbia = sach.Anhbia;
+            dDongia = double.Parse(sach.Giaban.ToString());
+            iSoluong = 1;
+        }
+    }
+}
